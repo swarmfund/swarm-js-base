10352e437d3c0eed6bde57767bf5d29d4b63d211
Dmitriy Haidashenko <d.s.gaydashenko@gmail.com>
2017-11-27 17:12:33 +0200

    Moved base operation with basic functions

---
+++ src/operations/base_operation.js
@@ -0,0 +1,259 @@
+import { default as xdr } from "../generated/stellar-xdr_generated";
+import { Keypair } from "../keypair";
+import { UnsignedHyper, Hyper } from "js-xdr";
+import { hash } from "../hashing";
+import { encodeCheck } from "../strkey";
+import BigNumber from 'bignumber.js';
+import { best_r } from "../util/continued_fraction";
+import padEnd from 'lodash/padEnd';
+import trimEnd from 'lodash/trimEnd';
+import isEmpty from 'lodash/isEmpty';
+import isUndefined from 'lodash/isUndefined';
+import isString from 'lodash/isString';
+import isNumber from 'lodash/isNumber';
+import isFinite from 'lodash/isFinite';
+import crypto from 'crypto';
+
+const ONE = 10000;
+const MAX_INT64 = '9223372036854775807';
+const MAX_INT64_AMOUNT = '922337203685477.5807';
+
+export class BaseOperation {
+
+    static isPayment(op) {
+        if (!(op instanceof xdr.Operation)) {
+            throw new Error('should be used for operations');
+        }
+        return op.body().switch().name === "payment";
+    }
+
+    static isValidAsset(value) {
+        return BaseOperation.isValidString(value, 1, 16);
+    }
+
+    static isValidString(value, minSize, maxSize) {
+        return isString(value) && value.length >= minSize && value.length <= maxSize;
+    }
+
+    static isValidSubject(value) {
+        return BaseOperation.isValidString(value, 0, 256);
+    }
+
+    static isValidArray(value, minSize) {
+        return Array.isArray(value) && value.length >= minSize;
+    }
+
+    static isValidArrayOfClass(value, minSize, cls) {
+        if (!BaseOperation.isValidArray(value, minSize)) {
+            return false;
+        }
+        for (var i = 0; i < value.length; i++) {
+            if (!(value[i] instanceof cls)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static isValidPeriod(value, allowZero = false) {
+        if (!isString(value)) {
+            return false;
+        }
+
+        let period;
+        try {
+            period = new BigNumber(value);
+        } catch (e) {
+            return false;
+        }
+
+        // == 0
+        if (!allowZero && period.isZero()) {
+            return false;
+        }
+
+        // < 0
+        if (period.isNegative()) {
+            return false;
+        }
+
+        if (period.decimalPlaces() > 0) {
+            return false;
+        }
+
+        // Infinity
+        if (!period.isFinite()) {
+            return false;
+        }
+
+        // NaN
+        if (period.isNaN()) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    static isValidAmount(value, allowZero = false, max = undefined) {
+        if (!isString(value)) {
+            return false;
+        }
+
+        let amount;
+        try {
+            amount = new BigNumber(value);
+        } catch (e) {
+            return false;
+        }
+
+        // == 0
+        if (!allowZero && amount.isZero()) {
+            return false;
+        }
+
+        // < 0
+        if (amount.isNegative()) {
+            return false;
+        }
+
+        // > Max value
+        if (amount.times(ONE).greaterThan(new BigNumber(MAX_INT64).toString())) {
+            return false;
+        }
+
+        if (max && amount.greaterThan(new BigNumber(max).toString())) {
+            return false;
+        }
+
+        // Decimal places (max 4)
+        if (amount.decimalPlaces() > 4) {
+            return false;
+        }
+
+        // Infinity
+        if (!amount.isFinite()) {
+            return false;
+        }
+
+        // NaN
+        if (amount.isNaN()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns value converted to uint32 value or undefined.
+     * If `value` is not `Number`, `String` or `Undefined` then throws an error.
+     * Used in {@link Operation.setOptions}.
+     * @private
+     * @param {string} name Name of the property (used in error message only)
+     * @param {*} value Value to check
+     * @param {function(value, name)} isValidFunction Function to check other constraints (the argument will be a `Number`)
+     * @returns {undefined|Number}
+     * @private
+     */
+    static _checkUnsignedIntValue(name, value, isValidFunction = null) {
+        if (isUndefined(value)) {
+            return undefined;
+        }
+
+        if (isString(value)) {
+            value = parseFloat(value);
+        }
+
+        if (!isNumber(value) || !isFinite(value) || value % 1 !== 0) {
+            throw new Error(`${name} value is invalid`);
+        }
+
+        if (value < 0) {
+            throw new Error(`${name} value must be unsigned`);
+        }
+
+        if (!isValidFunction ||
+            (isValidFunction && isValidFunction(value, name))) {
+            return value;
+        }
+
+        throw new Error(`${name} value is invalid`);
+    }
+
+    static calcPercentFee(amountValue, percentValue) {
+        let amount = new BigNumber(amountValue);
+        let rate = new BigNumber(percentValue).div(100);
+        return amount.times(rate).toString();
+    }
+
+    /**
+     * @private
+     */
+    static _toXDRAmount(value) {
+        let amount = new BigNumber(value).mul(ONE);
+        return Hyper.fromString(amount.toString());
+    }
+
+    /**
+     * @private
+     */
+    static _fromXDRAmount(value) {
+        return new BigNumber(value).div(ONE).toString();
+    }
+
+    /**
+     * @private
+     */
+    static _fromXDRPrice(price) {
+        let n = new BigNumber(price.n());
+        return n.div(new BigNumber(price.d())).toString();
+    }
+
+    /**
+     * @private
+     */
+    static _toXDRPrice(price) {
+        let xdrObject;
+        if (price.n && price.d) {
+            xdrObject = new xdr.Price(price);
+        } else {
+            price = new BigNumber(price);
+            let approx = best_r(price);
+            xdrObject = new xdr.Price({
+                n: parseInt(approx[0]),
+                d: parseInt(approx[1])
+            });
+        }
+
+        if (xdrObject.n() < 0 || xdrObject.d() < 0) {
+            throw new Error('price must be positive');
+        }
+
+        return xdrObject;
+    }
+
+    static _accountTypeFromNumber(rawAccountType) {
+        if (!BaseOperation._isValidAccountType(rawAccountType)) {
+            throw new Error(`XDR Read Error: Unknown AccountType member for value ${rawAccountType}`);
+        }
+
+        return xdr.AccountType._byValue.get(rawAccountType);
+    }
+
+    static _requestTypeFromNumber(rawRequestType) {
+        if (!BaseOperation._isValidRequestType(rawRequestType)) {
+            throw new Error(`XDR Read Error: Unknown RequestType member for value ${rawRequestType}`);
+        }
+
+        return xdr.RequestType._byValue.get(rawRequestType);
+    }
+
+
+    static _isValidAccountType(rawAccountType) {
+        return xdr.AccountType._byValue.has(rawAccountType);
+    }
+
+    static _isValidRequestType(rawRequestType) {
+        return xdr.RequestType._byValue.has(rawRequestType);
+    }
+}
