75cfb5ffac20c52ca1651567f1d5d7eb11ceb926
Dmtriiev Oleksandr <Lvbnhbr199809@gmail.com>
2018-04-17 16:37:11 +0300

    xdr upadted

--- src/generated/stellar-xdr_generated.js
+++ src/generated/stellar-xdr_generated.js
@@ -1,4 +1,4 @@
-// Automatically generated on 2018-04-13T16:48:02+03:00
+// Automatically generated on 2018-04-17T16:36:23+03:00
 // DO NOT EDIT or your changes may be overwritten
 
 /* jshint maxstatements:2147483647  */
